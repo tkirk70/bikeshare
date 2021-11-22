366fbb79c243f0eb8a20f0b8bcb996233b7e6914
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2021-11-22 18:25:38 -0500

    Third submission
    Adding filters for day, month, or all.

--- bikeshareworking.py
+++ bikeshareworking.py
@@ -48,7 +48,32 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
 
+   # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
+
+    # convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+
+    # extract month and day of week from Start Time to create new columns
+    df['month'] = df['Start Time'].dt.month
+    df['day_of_week'] = df['Start Time'].dt.weekday
+
+    # filter by month if applicable
+    if month != 'all':
+        # use the index of the months list to get the corresponding int
+        months = ['january', 'february', 'march', 'april', 'may', 'june']
+        month = months.index(month) + 1
+
+        # filter by month to create the new dataframe
+        df = df[df['month'] == month]
+
+    # filter by day of week if applicable
+    if day != 'all':
+        # filter by day of week to create the new dataframe
+        days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+        day = days.index(day)
+        df = df[df['day_of_week'] == day]
+    #filter by user type if applicable
     return df
 
 
@@ -62,8 +87,6 @@ def raw_data(df):
         view_display = input("Do you wish to see five more rows?: ").lower()
         if view_display != 'yes':
             break
-
-
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
